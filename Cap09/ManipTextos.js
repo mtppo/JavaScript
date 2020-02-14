0904e4859f51d707b706ae36df5d09429265ad8d
Marcio Tavares Pereira Pombo <marcio.tavares@ph3a.com.br>
2020-02-14 14:27:05 -0300

    Exemplo de concatenaca de strings

---
+++ Cap09/ManipTextos.js
@@ -0,0 +1,18 @@
+// Concatencao de String
+const login = 'ecmascript';
+const dia = '13 de setembro';
+const ano = 2016;
+
+const mensagem = 'Olá, ' + login + '\nHoje é: ' + dia + ' de ' + ano;
+
+console.log(mensagem);
+
+// Old School
+var nome = 'Diego';
+//console.log('Bem-vindo, ' + nome)
+
+// Modelo novo (Interpolação de String)
+console.log(`Bem-vindo, ${nome}`);
+
+const n1 = 1, n2 = 2;
+console.log(`${n1} + ${n2} = ${n1 + n2}`);
