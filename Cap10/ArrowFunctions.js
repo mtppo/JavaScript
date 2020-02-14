c243b6bdcfc693350e7a355644d61572be93a422
Marcio Tavares Pereira Pombo <marcio.tavares@ph3a.com.br>
2020-02-14 14:51:27 -0300

    Uso de funcoes fat arrow

---
+++ Cap10/ArrowFunctions.js
@@ -0,0 +1,16 @@
+// Utilizando Arrow Functions
+
+// declaração de funcção do Arrow
+var valor = (nome) => {
+    // corpo da função
+    return `Seja bem-vindo: ${nome}`;
+}
+
+console.log(valor);
+
+var boasVindas = nome => {
+    console.log("Seja bem-vindo, " + nome);
+}
+
+boasVindas("Marcio");
+
